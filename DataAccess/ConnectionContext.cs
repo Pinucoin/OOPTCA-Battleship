5fd7d1fd5d156b4577065caa28a6ade323302680
Pinucoin <damiannav03@gmail.com>
2023-12-23 13:20:49 +0100

    pt 2

---
+++ DataAccess/ConnectionContext.cs
@@ -0,0 +1,13 @@
+﻿using Common;
+
+namespace DataAccess
+{
+    public class ConnectionContext
+    {
+        public BattleshipDBEntities Context { get; set; }
+        public ConnectionContext()
+        {
+            Context = new BattleshipDBEntities();
+        }
+    }
+}
---
+++ DataAccess/PlayerRepository.cs
@@ -0,0 +1,26 @@
+﻿using Common;
+using System.Linq;
+
+namespace DataAccess
+{
+    public class PlayerRepository : ConnectionContext
+    {
+        public PlayerRepository() : base()
+        {
+
+        }
+
+        public IQueryable<Player> GetPlayers()
+        {
+            var players = from player in Context.Players
+                          select player;
+            return players.AsQueryable();
+        }
+
+        public void addPlayer(Player player)
+        {
+            Context.Players.Add(player);
+            Context.SaveChanges();
+        }
+    }
+}
